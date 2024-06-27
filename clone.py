d44223868dd97fd30c25b64d22eda1608a800c0b
FelixArgyleNya1 <felixargyle228a@gmail.com>
2024-06-27 23:17:07 +0300

    Update clone.py

--- clone.py
+++ clone.py
@@ -1,4 +1,5 @@
 from .. import loader, utils
+import asyncio
 
 class CloneChannelMod(loader.Module):
     """Clone messages from one channel to another"""
@@ -18,10 +19,22 @@ class CloneChannelMod(loader.Module):
         source_channel = args[0]
         target_channel = args[1]
         
-        async for msg in self.client.iter_messages(source_channel):
-            if msg.text:
-                await self.client.send_message(target_channel, msg.text)
-            elif msg.media:
-                await self.client.send_file(target_channel, msg.media)
-        
+        try:
+            async for msg in self.client.iter_messages(source_channel):
+                try:
+                    if msg.text:
+                        await self.client.send_message(target_channel, msg.text)
+                    elif msg.media:
+                        # Додайте затримку перед кожним відправленням медіафайлу
+                        await asyncio.sleep(2)  # Наприклад, затримка 2 секунди
+                        await self.client.send_file(target_channel, msg.media)
+                    
+                except Exception as e:
+                    await message.edit(f"<b>Failed to clone message: {str(e)}</b>")
+                    await asyncio.sleep(5)  # Затримка перед наступною спробою
+
+        except Exception as ex:
+            await message.edit(f"<b>Error: {str(ex)}</b>")
+            return
+
         await message.edit("<b>Cloning complete!</b>")
